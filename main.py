0c367cd4a745ee2fe68572d4c60f36ef1c3af121
Guilherme Lima <guirezendel@icloud.com>
2022-10-30 14:15:53 -0300

    Adicionei uma lista aos * para conseguir substituir quando acertar

--- main.py
+++ main.py
@@ -20,50 +20,62 @@ todasAsDicas.append(dica3)
 
 limparTela()
 
-converteAsterisco = tamanhoPalavra * "*"
-print("Quantidade de digitos da palavra:")
-print(converteAsterisco)
+palavraEmAsterisco = []
 
+for i in range(0,tamanhoPalavra):
+    palavraEmAsterisco.append("*")
+
+print("Quantidade de digitos da palavra:")
+print(palavraEmAsterisco)
 
 totalDeDicas = 0
 totalDeTentativas = 6
+
 while True:
     print("{}, qual opção deseja realizar?" .format(competidor))
     print("> Digite 1 para solicitar uma dica!")
     print("> Digite 2 para chutar uma letra!")
     print("> Digite 3 para começar novamente!")
+    
     try:
         escolha = int(input())
-
 
         if escolha == 1:
             if totalDeDicas == 0:
                 print("Sua primeira dica é:", todasAsDicas[0])
                 totalDeDicas += 1
+            
             elif totalDeDicas == 1:
                 print ("Sua segunda dica é:", todasAsDicas[1])
                 totalDeDicas  += 1
+            
             elif totalDeDicas == 2:
                 print("Sua terceira e última dica é:", todasAsDicas[2])
                 totalDeDicas += 1
+            
             else:
                 print("Você não tem mais direito a dicas!")
         
         
         elif escolha == 2:
+            letrasChutas = []
             print("Certo, qual letra você escolhe?")
             print("Você ainda tem {} tentativas" .format(totalDeTentativas))
             letraEscolhida = input(">> ").upper()
+            letrasChutas.append(letraEscolhida)
 
             if letraEscolhida in palavra and len(letraEscolhida) == 1:
                 print(palavra.index(letraEscolhida))
                 input()
+
             elif letraEscolhida not in palavra and len(letraEscolhida) == 1:
                 totalDeTentativas -= 1
                 print("Você errou! Restam apenas {} tentativas" .format(totalDeTentativas))
+                
                 if totalDeTentativas == 0:
                     print("{} venceu a partida! A palavra era {}" .format(desafiante, palavra))
                     break
+            
             else:
                 print("Somente é aceito letras! Não é possível chutar a palavra ou digitar mais de uma letra por vez!")
             
